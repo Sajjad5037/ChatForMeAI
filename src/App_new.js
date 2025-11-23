3911c844ea290661a02ccaeee7c768565b6f4996
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-11-23 20:53:10 +0500

    Update App_new.js

--- src/App_new.js
+++ src/App_new.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Link from "next/link";
 
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
